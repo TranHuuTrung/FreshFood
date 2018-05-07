d6d466001bad4a49b9d22ecb7b5cd78eabbe1c4b
TranHuuTrung <tranhuutrung1408@gmail.com>
2018-05-08 02:35:47 +0700

    create 2 pages: category and details product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    var headerUrl        = "./snippets/header.html";
-    var sliderUrl        = "./snippets/slider.html";
-    var categoryUrl      = "./snippets/category.html";
-    var handbookUrl      = "./snippets/handbook.html";
-    var footerUrl        = "./snippets/footer.html";
+    // var headerUrl        = "./snippets/header.html";
+    // var sliderUrl        = "./snippets/slider.html";
+    // var categoryUrl      = "./snippets/category.html";
+    // var handbookUrl      = "./snippets/handbook.html";
+    // var footerUrl        = "./snippets/footer.html";
 
 
-    //function for insert innerHTML for 'select'
-    var insertHtml = function (selector, html_url){
-        $(selector).load(html_url); 
-     }
-    //include header
-    insertHtml(".nav-header", headerUrl);
-    //include slider
-    insertHtml(".sliderHeader", sliderUrl);
-    //include category
-    insertHtml(".category", categoryUrl);
-    // include handbook
-    insertHtml(".handbook", handbookUrl);
-    // include footer
-    insertHtml(".footer", footerUrl);
-    // //load index page when click logo
+    // //function for insert innerHTML for 'select'
+    // var insertHtml = function (selector, html_url){
+    //     $(selector).load(html_url); 
+    //  }
+    // //include header
+    // insertHtml(".nav-header", headerUrl);
+    // //include slider
+    // insertHtml(".sliderHeader", sliderUrl);
+    // //include category
+    // insertHtml(".category", categoryUrl);
+    // // include handbook
+    // insertHtml(".handbook", handbookUrl);
+    // // include footer
+    // insertHtml(".footer", footerUrl);
+    // // //load index page when click logo
     // $(".logo").click(function(){
     //   insertHtml(".content", startUrl);
     // });
@@ -38,7 +38,12 @@ $(document).ready(function(){
     // $("#login").click(function(){
     //   insertHtml(".content", loginUrl);
     // });
-
+    // function Redirect() {
+    //     window.location="http://www.vietjack.com";
+    // }
+    // $('.linktoCategory').click(function(){
+    //     alert("Clicked");
+    // });
     
 
 });
